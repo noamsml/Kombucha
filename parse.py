6a724575be3d118e697ed66ab653e496b0a6dc8e
Noam Samuel <noam@athena.(none)>
2011-01-06 21:27:42 -0500

    Tokenizing works! :D

--- parse.py
+++ parse.py
@@ -1,12 +1,29 @@
 import ply.lex as lex
 
 def OuterTokenizer():
-	tokens = ( 'CLOSETAG', 'OPENCLOSETAG', 'TAG', 'HASHVAR', 'NORMSTR' )
+	tokens = ( 'TAGOPEN', 'TAGCLOSE', 'TAGNAME', 'SLASH', 'NORMSTR', 'HASHVAR', 'TAG_WS' )
+	states = (
+		("intag", "exclusive"),
+	)
 	
-	t_CLOSETAG = r'\<@\W*/\W*[a-zA-Z0-9_]+\W*@\'\>'
-	t_OPENCLOSETAG = r'\<@\W*[a-zA-Z0-9_]+\W*/\W*@\>'
-	t_TAG = r'\<@\W*[a-zA-Z0-9_]+\W*@\>' #for now
+	def t_TAGOPEN(t):
+			r'\<@'
+			t.lexer.begin("intag")
+			return t
+
+	def t_intag_TAGCLOSE(t):
+			r'@\>'
+			t.lexer.begin("INITIAL")
+			return t
+			
+	t_intag_SLASH = r'/'
 	t_HASHVAR = r'@[a-zA-Z0-9_]+@'
+	
+	def t_intag_TAG_WS(t):
+		r'[ \t]+'
+		pass
+	
+	t_intag_TAGNAME = r'[a-zA-Z0-9_]+'
 	t_NORMSTR = r'(\<[^@]|\<$|[^<@])+'
 	
 	return lex.lex()
